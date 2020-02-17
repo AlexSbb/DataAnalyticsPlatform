27d9a173adad284731dc753c619fa6d321bba70b
Srinidhi Subbanna <srinidhi.subbanna.ms19@gmail.com>
2020-02-17 16:03:55 +0100

    Added Input and output STRUCT for Random Forest
    V1.0.1

--- pycode/RandomForestFn.py
+++ pycode/RandomForestFn.py
@@ -1,31 +1,61 @@
 import numpy as np 
-import matplotlib.pyplot as plt 
+#import matplotlib.pyplot as plt 
 import pandas as pd 
+from typing import NamedTuple
 
-X = [   [4.5,6.7],
+# Input structure for Neural Network
+class RF_inputs(NamedTuple):
+    X: float
+    y: float
+    trees: int
+    tst_siz: float
+   
+# Output structure for Neural Network
+class RF_outputs(NamedTuple):
+    y_test: float
+    X_test: float 
+    y_actual: float
+    length: int
+    tst_mse: float
+    tst_accrc: float
+
+# input dataset 
+X1 = [   [4.5,6.7],
         [8.9,3.8],
         [9,6.8],
         [12.3,8.8] ]
 
 # output dataset            
-y = [8.55055,5.53195,9.1547,11.91745]
+y1 = [8.55055,5.53195,9.1547,11.91745]
 
-# Fitting Random Forest Regression to the dataset 
-# import the regressor 
+# Input initialization
+RF_inputs1 = RF_inputs(X1,y1,1000,0.2)
 
-def RFreg(est,tst_siz):
+# Random Forest regressor function 
+def RFreg(RF_inputs):
     from sklearn.ensemble import RandomForestRegressor 
     #regressor = RandomForestRegressor(n_estimators = 100, random_state = 0) 
-    regressor = RandomForestRegressor(n_estimators = est, random_state = 42) 
+    regressor = RandomForestRegressor(n_estimators = RF_inputs.trees, random_state = 42) 
     #Train the model with training data and test it with test data  (80% train and 20% test)
     from sklearn.model_selection import train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= tst_siz, random_state=40)
+    from sklearn.metrics import mean_squared_error
+    X_train, RF_outputs.X_test, y_train, RF_outputs.y_actual = train_test_split(RF_inputs.X, RF_inputs.y, test_size= RF_inputs.tst_siz, random_state=40)
     # fit the regressor with x and y data 
     regressor.fit(X_train, y_train)   
     # test the output by changing values 
-    y_pred = regressor.predict(X_test)  
-    return y_pred, X_test
+    RF_outputs.y_test = regressor.predict(RF_outputs.X_test)
+    RF_outputs.length = len(RF_outputs.X_test)
+    RF_outputs.tst_mse = mean_squared_error(RF_outputs.y_actual, RF_outputs.y_test)
+    RF_outputs.tst_accrc = 100.0 -  RF_outputs.tst_mse
+    return RF_outputs
 
-y_new, x_new = RFreg(1000,0.2)
-print(x_new)
-print(y_new)
+# Function call
+RF_outputs1 = RFreg(RF_inputs1)
+
+#Printing outputs
+print('Predicted Output:       ', RF_outputs1.y_test)
+print('test input:             ', RF_outputs1.X_test)
+print('expected output:        ', RF_outputs1.y_actual)
+print('length of output array: ', RF_outputs1.length)
+print('Mean Square error:      ', RF_outputs1.tst_mse)
+print('Accuracy:               ', RF_outputs1.tst_accrc)
